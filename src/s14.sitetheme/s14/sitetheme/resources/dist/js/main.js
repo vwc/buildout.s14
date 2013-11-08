bb97a7a6a44b9d7740521649f1e4b97d5d545e7c
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-11-08 18:42:46 +0100

    Prepare missing event integration and navigation

--- src/s14.sitetheme/s14/sitetheme/resources/dist/js/main.js
+++ src/s14.sitetheme/s14/sitetheme/resources/dist/js/main.js
@@ -2012,11 +2012,11 @@ if (typeof jQuery === "undefined") { throw new Error("Bootstrap requires jQuery"
 
 /*jslint white:false, onevar:true, undef:true, nomen:true, eqeqeq:true, plusplus:true, bitwise:true, regexp:true, newcap:true, immed:true, strict:false, browser:true */
 /*global jQuery:false, document:false */
+'use strict';
 
 (function ($) {
     $(document).ready(function () {
-        if (jQuery.browser.msie && parseInt(jQuery.browser.version, 10) < 7) {return;
-        }
+        if ($('body').hasClass('lt-ie7')) {return; }
         // Application specific javascript code goes here
     }
     );

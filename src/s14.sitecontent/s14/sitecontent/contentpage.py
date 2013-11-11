b5a5e2368ef55d67cf203cb0068bc50ddee15851
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-11-11 17:14:44 +0100

    Update to restyle the event listing

--- src/s14.sitecontent/s14/sitecontent/contentpage.py
+++ src/s14.sitecontent/s14/sitecontent/contentpage.py
@@ -2,19 +2,11 @@ from five import grok
 
 from z3c.form import group, field
 from zope import schema
-from zope.interface import invariant, Invalid
-from zope.schema.interfaces import IContextSourceBinder
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
 from plone.dexterity.content import Container
 from plone.directives import dexterity, form
 from plone.app.textfield import RichText
-from plone.namedfile.field import NamedImage, NamedFile
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
 from plone.namedfile.interfaces import IImageScaleTraversable
-
-from z3c.relationfield.schema import RelationList, RelationChoice
-from plone.formwidget.contenttree import ObjPathSourceBinder
 
 
 from s14.sitecontent import MessageFactory as _
@@ -24,6 +16,10 @@ class IContentPage(form.Schema, IImageScaleTraversable):
     """
     Folderish Content Pages
     """
+    text = RichText(
+        title=_(u"Body Text"),
+        required=False,
+    )
 
 
 class ContentPage(Container):

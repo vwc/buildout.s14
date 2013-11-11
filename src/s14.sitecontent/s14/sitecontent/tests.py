1389d80b0ffb5d01c4d18ca40c50dd7a34dabdfb
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-11-11 14:45:26 +0100

    Add dedicated site content package

---
+++ src/s14.sitecontent/s14/sitecontent/tests.py
@@ -0,0 +1,58 @@
+import doctest
+import unittest
+
+from Testing import ZopeTestCase as ztc
+
+from Products.Five import zcml
+from Products.PloneTestCase import PloneTestCase as ptc
+from Products.PloneTestCase.layer import PloneSite
+from Products.PloneTestCase.layer import onsetup
+
+import s14.sitecontent
+
+OPTION_FLAGS = doctest.NORMALIZE_WHITESPACE | \
+               doctest.ELLIPSIS
+
+ptc.setupPloneSite(products=['s14.sitecontent'])
+
+
+class TestCase(ptc.PloneTestCase):
+
+    class layer(PloneSite):
+
+        @classmethod
+        def setUp(cls):
+            zcml.load_config('configure.zcml',
+              s14.sitecontent)
+
+        @classmethod
+        def tearDown(cls):
+            pass
+
+
+def test_suite():
+    return unittest.TestSuite([
+
+        # Unit tests
+        #doctestunit.DocFileSuite(
+        #    'README.txt', package='s14.sitecontent',
+        #    setUp=testing.setUp, tearDown=testing.tearDown),
+
+        #doctestunit.DocTestSuite(
+        #    module='s14.sitecontent.mymodule',
+        #    setUp=testing.setUp, tearDown=testing.tearDown),
+
+
+        # Integration tests that use PloneTestCase
+        ztc.ZopeDocFileSuite(
+            'INTEGRATION.txt',
+            package='s14.sitecontent',
+            optionflags = OPTION_FLAGS,
+            test_class=TestCase),
+
+        # -*- extra stuff goes here -*-
+
+        ])
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')

535617e99012c9b176c069ce3c676e9b641ed9b0
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-11-11 16:30:29 +0100

    Add content type

--- src/s14.sitecontent/s14/sitecontent/tests.py
+++ src/s14.sitecontent/s14/sitecontent/tests.py
@@ -52,6 +52,14 @@ def test_suite():
 
         # -*- extra stuff goes here -*-
 
+        # Integration tests for ContentPage
+        ztc.ZopeDocFileSuite(
+            'ContentPage.txt',
+            package='s14.sitecontent',
+            optionflags = OPTION_FLAGS,
+            test_class=TestCase),
+
+
         ])
 
 if __name__ == '__main__':

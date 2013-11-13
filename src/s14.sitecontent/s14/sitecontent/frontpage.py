a520bdda1f7b2866a85e69b05a41174e4713e7dd
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-11-13 15:20:51 +0100

    Add editable frontpage view

---
+++ src/s14.sitecontent/s14/sitecontent/frontpage.py
@@ -0,0 +1,35 @@
+from five import grok
+from plone import api
+
+from plone.app.contentlisting.interfaces import IContentListing
+from plone.event.interfaces import IEvent
+from plone.app.contenttypes.interfaces import IDocument
+from vwc.blog.blogentry import IBlogEntry
+
+
+class FrontPageView(grok.View):
+    grok.context(IDocument)
+    grok.require('zope2.View')
+    grok.name('frontpage-view')
+
+    def update(self):
+        self.has_events = len(self.recent_events()) > 0
+        self.has_blogentries = len(self.blogentries()) > 0
+
+    def blogentries(self):
+        catalog = api.portal.get_tool(name='portal_catalog')
+        items = catalog(object_provides=IBlogEntry.__identifier__,
+                        review_state='published',
+                        sort_on='modified',
+                        sort_limit=2)[:2]
+        results = IContentListing(items)
+        return results
+
+    def recent_events(self):
+        catalog = api.portal.get_tool(name='portal_catalog')
+        items = catalog(object_provides=IEvent.__identifier__,
+                        review_state='published',
+                        sort_on='modified',
+                        sort_limit=2)[:2]
+        results = IContentListing(items)
+        return results
